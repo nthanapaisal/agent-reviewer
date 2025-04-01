c25210d8345937e7f057523198ebb99bd77fc906
bryansmith00 <bryansmith8023@gmail.com>
2025-04-01 15:14:41 -0500

    initial Dockerfile

--- src/backend/transcription/transcription.py
+++ src/backend/transcription/transcription.py
@@ -4,6 +4,8 @@ def transcribe_file(filepath: str) -> str:
     evaluate if you need to import model everytime you got the api call 
     should you have it install when docker image was built. 
     """
+    # list of tuples with key as speaker and value as the line of text, ordered
+    
     print(f"Transcribing file: {filepath}")
 
     return f"Dummy transcription for file: {filepath}"
