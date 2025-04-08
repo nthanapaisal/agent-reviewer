e9a157e4c81088d0db8a8d4718d8a8b499fac723
bryansmith00 <bryansmith8023@gmail.com>
2025-04-07 20:49:23 -0500

    diarization and transcription

--- src/backend/transcription/transcription.py
+++ src/backend/transcription/transcription.py
@@ -1,23 +1,87 @@
 import whisper
+from pyannote.audio import Pipeline
+import os
+from dotenv import load_dotenv
+from pydub import AudioSegment
+import re
+import datetime
 
-def transcribe_file(filepath: str) -> str:
-    """
-    you are given path, use model like whisper to transcribe it. 
-    evaluate if you need to import model everytime you got the api call 
-    should you have it install when docker image was built. 
-    """
+"""
+Input: 
+    string path to an audio file
+Output:
+    (
+    Transcription: list of tuples (speaker, text),
+    datetime: datetime object,
+    duration length of the original audio file in milliseconds (ms):
+    )
+"""
+def transcribe_file(filepath: str):
+    time = datetime.datetime.now()
+    temp_name = "audio.wav"
     
-    """
-    {
-    Transcription:,
-    datetime:,
-    duration:
-    }
-    where transcription is a list of tuples with key as speaker and value as line of text of that speaker
-    """
+    audio = AudioSegment.from_file(filepath)
+    duration = len(audio)
     
-    model = whisper.load_model("tiny")
+    file = pad_audio(filepath, temp_name)
     
-    print(f"Transcribing file: {filepath}")
+    load_dotenv()
+    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token=os.getenv("HUGGING_FACE"))
+    dz = pipeline(file)
+    
+    with open("diarization.txt", "w") as text_file:
+        text_file.write(str(dz))
 
-    return f"Dummy transcription for file: {filepath}"
+    # extract the diarization data into an easily parsable list
+    dz = open('diarization.txt').read().splitlines()
+    dzList = []
+    for l in dz:
+        start, end =  tuple(re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=l))
+        start = millisec(start)
+        end = millisec(end)
+        lex = re.findall('\sSPEAKER_(\d\d)', string=l)
+        dzList.append((start, end, lex[0]))
+    
+    print("---------------------") 
+    print("Log: diarization done")
+    print("---------------------")
+    
+    # available models: tiny, base, small, medium, large, turbo
+    model = whisper.load_model("small")
+    
+    # for each segment transcribe it and append to the list
+    transcription = []
+    print("Log: adding items to transcription list")
+    for item in dzList:
+        audio = AudioSegment.from_wav(file)
+        a = audio[item[0]: item[1]]
+        a.export("a.wav", format="wav")
+        result = model.transcribe("a.wav")
+        transcription.append((item[2], result["text"]))
+    
+    print("-----------------------")
+    print("Log: transcription done")
+    print("-----------------------")
+    
+    # clean up temp files
+    os.remove("a.wav")
+    os.remove("audio.wav")
+    os.remove("diarization.txt")
+
+    return (transcription, time, duration)
+
+# adds a blank 2 second pad to the beginning of the clip to ensure the model doesn't miss any audio
+def pad_audio(filepath, output_name="audio.wav"):    
+    audio = AudioSegment.from_file(filepath)
+    spacermilli = 2000
+    spacer = AudioSegment.silent(duration=spacermilli)
+    audio = spacer.append(audio, crossfade=0)
+
+    audio.export(output_name, format='wav')
+    
+    return output_name
+
+def millisec(timeStr):
+    spl = timeStr.split(":")
+    s = (int)((int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2]) )* 1000)
+    return s
